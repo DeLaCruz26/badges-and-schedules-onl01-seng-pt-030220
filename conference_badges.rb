6b9f3ef927f6f6b30df6441d7a2c2563781e3dd7
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-11 04:28:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |name| badge_maker(name)
+  end
+end
+
+def assign_rooms(attendees)
+  new_array = []
+  attendees.each_with_index do |name, index| new_array << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+  new_array
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
+end
